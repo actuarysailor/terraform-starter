3259b17b52da2106cb01a2e7ea9e39efca8a59c5
actuarysailor <24359398+actuarysailor@users.noreply.github.com>
2024-08-20 12:21:19 -0400

    fix: Issues with template

--- policies.tf
+++ policies.tf
@@ -1,3 +1,26 @@
+# ACCESS POLICY
+#
+# This example access policy gives everyone in the "Engineering" GitHub team
+# read access to the stack.
+#
+# You can read more about access policies here:
+#
+# https://docs.spacelift.io/concepts/policy/stack-access-policy
+# Seems like SpaceLift.IO deprecated policies
+# https://docs.spacelift.io/concepts/policy/stack-access-policy
+resource "spacelift_policy" "access" {
+  type = "ACCESS"
+
+  name = "All of Engineering gets read access"
+  body = file("${path.module}/policies/access.rego")
+}
+
+# Access policies only take effect when attached to the stack.
+resource "spacelift_policy_attachment" "access" {
+  policy_id = spacelift_policy.access.id
+  stack_id  = spacelift_stack.managed.id
+}
+
 # PLAN POLICY
 #
 # This example plan policy prevents you from creating weak passwords, and warns 
@@ -85,6 +108,9 @@ resource "spacelift_policy_attachment" "trigger-self" {
 # 
 # https://docs.spacelift.io/concepts/policy/login-policy
 resource "spacelift_policy" "login" {
+  # Login policies are only evaluated for the Cloud or Enterprise plan.
+  
+
   type = "LOGIN"
 
   name = "Standard GitHub login policy"
